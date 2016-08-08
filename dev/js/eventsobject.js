6c053d1e6a1ede08c26cb76442a4471b7f39bbdf
Lukas Hermann <lukher6@web.de>
2016-08-08 20:18:27 +0200

    Implemented interactive calendar

---
+++ dev/js/eventsobject.js
@@ -0,0 +1,94 @@
+var events = [
+	{
+		time: new Date(2016, 8, 23, 19, 00),
+		title: "Die Strahlen der Ewigkeit"
+	},
+	{
+		time: new Date(2016, 8, 24, 18, 00),
+		title: "Der Biss der Schlange"
+	},
+	{
+		time: new Date(2016, 8, 24, 19, 45),
+		title: "Der Untergang der Alten Welt"
+	},
+	{
+		time: new Date(2016, 8, 26, 19, 00),
+		title: "Der Patriarch von Uru"
+	},
+	{
+		time: new Date(2016, 8, 29, 19, 00),
+		title: "An den Brunnen von Aram"
+	},
+	{
+		time: new Date(2016, 8, 30, 19, 00),
+		title: "Im Schatten der Pyramiden"
+	},
+	{
+		time: new Date(2016, 9, 1, 18, 00),
+		title: "..."
+	},
+	{
+		time: new Date(2016, 9, 1, 19, 45),
+		title: "..."
+	},
+	{
+		time: new Date(2016, 9, 3, 19, 00),
+		title: "..."
+	},
+	{
+		time: new Date(2016, 9, 5, 19, 00),
+		title: "..."
+	},
+	{
+		time: new Date(2016, 9, 7, 19, 00),
+		title: "..."
+	},
+	{
+		time: new Date(2016, 9, 8, 18, 00),
+		title: "..."
+	},
+	{
+		time: new Date(2016, 9, 8, 19, 45),
+		title: "..."
+	},
+	{
+		time: new Date(2016, 9, 10, 19, 00),
+		title: "..."
+	},
+	{
+		time: new Date(2016, 9, 12, 19, 00),
+		title: "..."
+	},
+	{
+		time: new Date(2016, 9, 14, 19, 00),
+		title: "..."
+	},
+	{
+		time: new Date(2016, 9, 15, 18, 00),
+		title: "..."
+	},
+	{
+		time: new Date(2016, 9, 15, 19, 45),
+		title: "..."
+	},
+	{
+		time: new Date(2016, 9, 17, 19, 00),
+		title: "..."
+	},
+	{
+		time: new Date(2016, 9, 19, 19, 00),
+		title: "..."
+	},
+	{
+		time: new Date(2016, 9, 21, 19, 00),
+		title: "..."
+	},
+	{
+		time: new Date(2016, 9, 22, 18, 00),
+		title: "..."
+	},
+	{
+		time: new Date(2016, 9, 22, 19, 45),
+		title: "..."
+	}
+]
--- dev/js/script.js
+++ dev/js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+
+    /*
+     * Initiating the FlipClock Countdown
+     */
     var clock;
 
     // Initiate FlipClock
@@ -22,5 +26,42 @@ $(document).ready(function() {
     // Start the countdown
     clock.setTime(difference);
     clock.start();
+
+
+    /*
+     * Loop through the calendar days and insert the Livestream event
+     */
+    var calendar = $('#calendar');
+    $('#calendar').find('td').each(function(){
+        var id = $(this).attr("id");
+        if( id !== undefined ) {
+            // console.log(id);
+            var i;
+            var day = id.split("/")
+            var title = "";
+
+            for (i = 0; i < events.length; ++i) {
+
+                var e = events[i];
+                if( e.time.toDateString() === new Date(2016, day[0]-1, day[1]).toDateString() ) {
+
+                    if( title ) title += "<br>";
+                    var time = "<span class='color-primary'>" + e.time.getHours() + ":" + ("0" + e.time.getMinutes()).slice(-2) + "</span>";
+                    title += time + " " + (i+1) + ". " + e.title;
+                    // console.log(events[i]);
+
+                }
+            }
+
+            if( title ) {
+                $(this).html('<span class="eventday" data-toggle="tooltip" title="'+title+'">'+day[1]+'</span>');
+            }
+
+        }
+
+    });
+
+
+    $('.eventday').tooltip({'html': true})
 
 });
