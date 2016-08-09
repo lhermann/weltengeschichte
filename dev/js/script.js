695b66f223e9c95613f476c084b19c6f306914d7
Lukas Hermann <lukher6@web.de>
2016-08-09 11:12:34 +0200

    Finished markup, styling and js for termin section

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,62 +1,95 @@
 $(document).ready(function() {
+    /*
+     * Reused variables
+     */
+    // var now = new Date();
+    var now = new Date(2016, 8, 13, 11, 20);
+
+
+    // /*
+    //  * Initiating the FlipClock Countdown
+    //  */
+    // var clock;
+
+    // // Initiate FlipClock
+    // clock = $('.countdown__inner').FlipClock({
+    //     clockFace: 'DailyCounter',
+    //     countdown: true,
+    //     language: "de",
+    //     autoStart: false,
+    //     callbacks: {
+    //         stop: function() {
+    //             $('.message').html('The clock has stopped!')
+    //         }
+    //     }
+    // });
+
+    // // Calculate Seconds until next presentation
+    // var next = new Date(2016, 8, 23, 19, 00);
+    // var difference = (next - now) / 1000;
+
+    // // Start the countdown
+    // clock.setTime(difference);
+    // clock.start();
+
+
 
     /*
-     * Initiating the FlipClock Countdown
+     * Populate Themenlist
      */
-    var clock;
+    var tl = $('#themenlist');
+    var counter = 0;
+    for (var i = 0; i < events.length; ++i) {
 
-    // Initiate FlipClock
-    clock = $('.countdown__inner').FlipClock({
-        clockFace: 'DailyCounter',
-        countdown: true,
-        language: "de",
-        autoStart: false,
-        callbacks: {
-            stop: function() {
-                $('.message').html('The clock has stopped!')
-            }
+        // Logical checks
+        if(events[i].time < now) continue;
+        counter++;
+        if(counter > 5) {
+            if(counter === 6) $(tl).append( $('<li class="list-group-item">...</li>') );
+            continue;
         }
-    });
 
-    // Calculate Seconds until next presentation
-    var now = new Date();
-    var next = new Date(2016, 8, 23, 19, 00);
-    console.log(now);
-    console.log(next);
-    var difference = (next - now) / 1000;
-
-    // Start the countdown
-    clock.setTime(difference);
-    clock.start();
+        // Generate list item
+        var e = events[i];
+        var days = ["So.", "Mo.", "Di.", "Mi.", "Do.", "Fr.", "Sa."];
+        var time = '<span class="badge">' + days[e.time.getDay()] + ' ' + e.time.getDate() + '.' + (e.time.getMonth()+1) + '. &bull; ' + e.time.getHours() + ":" + ("0" + e.time.getMinutes()).slice(-2) + '</span>';
+        var li = $( '<li class="list-group-item">' + time + (i+1) + '. ' + e.title + '</li>' );
+        $(tl).append(li);
+    }
 
 
     /*
      * Loop through the calendar days and insert the Livestream event
      */
-    var calendar = $('#calendar');
     $('#calendar').find('td[id]').each(function(){
         var id = $(this).attr("id");
+        var calday = id.split("/");
+        var caldate = new Date(2016, calday[0]-1, calday[1]);
+
+        // Highlight Today
+        if( now.toDateString() === caldate.toDateString() ) {
+            $(this).addClass('today');
+        }
+
+        // Insert events
         if( id !== undefined ) {
-            // console.log(id);
-            var i;
-            var day = id.split("/")
+
             var title = "";
 
-            for (i = 0; i < events.length; ++i) {
+            for (var i = 0; i < events.length; ++i) {
 
                 var e = events[i];
-                if( e.time.toDateString() === new Date(2016, day[0]-1, day[1]).toDateString() ) {
+                if( e.time.toDateString() === caldate.toDateString() ) {
 
                     if( title ) title += "<br>";
                     var time = "<span class='color-primary'>" + e.time.getHours() + ":" + ("0" + e.time.getMinutes()).slice(-2) + "</span>";
                     title += time + " " + (i+1) + ". " + e.title;
-                    // console.log(events[i]);
 
                 }
             }
 
             if( title ) {
-                $(this).html('<span class="eventday" data-toggle="tooltip" title="'+title+'">'+day[1]+'</span>');
+                $(this).html('<span class="eventday" data-toggle="tooltip" title="'+title+'">'+calday[1]+'</span>');
             }
 
         }
