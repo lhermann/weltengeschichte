d1c5b51a91b162b371e5392eacb491c28cc71299
Lukas Hermann <lukher6@web.de>
2016-08-09 16:03:39 +0200

    Finished countdown/cover css, markup, js

--- dev/js/script.js
+++ dev/js/script.js
@@ -2,58 +2,109 @@ $(document).ready(function() {
     /*
      * Reused variables
      */
-    var now = new Date();
-    // var now = new Date(2016, 8, 24, 19, 44);
+    // var now = new Date();
+    var now = getNow();
+
+    function getNow() {
+        return new Date();
+        // return new Date(2016, 8, 23, 18, 59, 53);
+    }
 
 
     /*
-     * Determine next and currently active event
+     * Determine the next and currently active event
      */
     var nextEvent,
         nextEventIndex,
         activeEvent,
-        activeEventIndex,
-        nowLess90Min = new Date(now.getTime()-(90*60*1000));
+        activeEventIndex;
 
-    for (var i = 0; i < events.length; ++i) {
-        if(events[i].time < now && events[i].time > nowLess90Min) {
-            activeEvent = events[i];
-            activeEventIndex = i;
-            continue;
-        } else if(events[i].time < now) {
-            continue;
-        } else {
-            nextEvent = events[i];
-            nextEventIndex = i;
-            break;
+    updateEventVars(now);
+    function updateEventVars(now) {
+        var nowLess90Min = new Date(now.getTime()-(90*60*1000));
+
+        for (var i = 0; i < events.length; ++i) {
+            if(events[i].time < now && events[i].time > nowLess90Min) {
+                activeEvent = events[i];
+                activeEventIndex = i;
+                continue;
+            } else if(events[i].time < now) {
+                continue;
+            } else {
+                nextEvent = events[i];
+                nextEventIndex = i;
+                break;
+            }
         }
     }
 
 
     /*
-     * Initiating the FlipClock Countdown
+     * Set Up the FlipClock Countdown
      */
-    var clock;
-
-    // Initiate FlipClock
-    clock = $('#countdownTimer').FlipClock({
+    var clock = $('#countdownTimer').FlipClock({
         clockFace: 'DailyCounter',
         countdown: true,
         language: "de",
         autoStart: false,
         callbacks: {
             stop: function() {
-                $('.message').html('The clock has stopped!')
+                setTimeout(function(){
+                    reinitializeAppropriateDisplay()
+                }, 1100);
             }
         }
     });
 
-    // Calculate Seconds until next presentation
-    var difference = (nextEvent.time - now) / 1000;
 
-    // Start the countdown
-    clock.setTime(difference);
-    clock.start();
+    /*
+     * Detemine which display to show on the page
+     */
+    displayOnPage();
+    function displayOnPage() {
+        if( activeEvent ) {
+
+            updateEpisode(activeEvent, activeEventIndex);
+            $('#countdown').addClass('hidden');
+            $('#liveEpisode').removeClass('hidden');
+
+        } else if( nextEvent ) {
+
+            updateEpisode(nextEvent, nextEventIndex);
+            $('#liveEpisode').addClass('hidden');
+            $('#countdown').removeClass('hidden');
+
+            // Start the countdown
+            clock.setTime(Math.ceil((nextEvent.time - now) / 1000));
+            clock.start();
+
+        }
+    }
+
+    /*
+     * Function to update the prominently displayed episode title and number
+     */
+    function updateEpisode(episode, episodeIndex) {
+        $('.episodeNumber')
+            .text(episodeIndex+1)
+            .next().text(episode.title);
+    }
+
+    /*
+     * Recalculate present time and reinitialize appropriate display
+     */
+    function reinitializeAppropriateDisplay() {
+        now = getNow();
+        updateEventVars(now);
+        displayOnPage();
+    }
+
+    /*
+     * Do the reinitializing thing every 5 minutes
+     */
+    setInterval(function() {
+        reinitializeAppropriateDisplay()
+    }, 5*60*1000);
 
 
 
@@ -62,7 +113,7 @@ $(document).ready(function() {
      */
     var tl = $('#themenlist');
     var counter = 0;
-    for (var i = nextEventIndex; i < events.length; ++i) {
+    for (var i = nextEventIndex ? nextEventIndex : 0 ; i < events.length; ++i) {
 
         // Limit number of events listed
         counter++;
@@ -89,7 +140,6 @@ $(document).ready(function() {
         var caldate = new Date(2016, calday[0]-1, calday[1]);
 
         // Highlight Today
-        // console.log(now.toDateString() + ' --- ' + caldate.toDateString());
         if( now.toDateString() === caldate.toDateString() ) {
             $(this).addClass('today');
         }
