840a595eeef1ae55b8213ebe195eab7011d6c69b
Lukas Hermann <lukher6@web.de>
2016-08-10 17:09:02 +0200

    Deploy on push, test 3

--- dev/deploy.php
+++ dev/deploy.php
@@ -1,7 +1,7 @@
 <?php
     // Use in the "Post-Receive URLs" section of your GitHub repo.
     if ( $_POST['payload'] && false ) {
-        file_put_contents('log/payload.txt', print_r($_POST['payload'], FILE_APPEND);
+        file_put_contents('log/payload.txt', print_r($_POST['payload']), FILE_APPEND);
 
         $exec_string = 'cd /var/customers/webs/jmm/weltengeschichte.de.repo/ && git reset --hard HEAD && git pull';
         exec ( $exec_string, $output);
