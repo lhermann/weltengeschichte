7e77ea4d77fe1d081c108e7eba8f6e08e343ab21
Lukas Hermann <lukher6@web.de>
2016-08-10 16:01:37 +0200

    Added deploy script

---
+++ dev/deploy.php
@@ -0,0 +1,11 @@
+<?php
+    // Use in the "Post-Receive URLs" section of your GitHub repo.
+    if ( $_POST['payload'] ) {
+        file_put_contents('log/payload.txt', print_r($_POST['payload'], FILE_APPEND);
+
+        $exec_string = 'cd /var/customers/webs/jmm/weltengeschichte.de.repo/ && git reset --hard HEAD && git pull';
+        exec ( $exec_string, $output);
+        file_put_contents('log/output.txt', $output, FILE_APPEND);
+    }
+    file_put_contents('log/post.txt', print_r($_POST), FILE_APPEND);
+?>
