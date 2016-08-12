eb70380f22eb7dbbb8a4b29bebabec5115684257
Lukas Hermann <lukher6@web.de>
2016-08-12 14:48:17 +0200

    Added script for google maps scroll

--- dev/js/script.js
+++ dev/js/script.js
@@ -172,4 +172,38 @@ $(document).ready(function() {
 
     $('.eventday').tooltip({'html': true})
 
+
+
+    /**
+     * Disable interaction with google maps until
+     * it is being clicked upon directly
+     *
+     * @source http://stackoverflow.com/a/25904582/3346744
+     */
+    // Disable scroll zooming and bind back the click event
+    var onMapMouseleaveHandler = function (event) {
+        var that = $(this);
+
+        that.on('click', onMapClickHandler);
+        that.off('mouseleave', onMapMouseleaveHandler);
+        that.find('iframe').css("pointer-events", "none");
+    }
+
+    var onMapClickHandler = function (event) {
+        var that = $(this);
+
+        // Disable the click handler until the user leaves the map area
+        that.off('click', onMapClickHandler);
+
+        // Enable scrolling zoom
+        that.find('iframe').css("pointer-events", "auto");
+
+        // Handle the mouse leave event
+        that.on('mouseleave', onMapMouseleaveHandler);
+    }
+
+    // Enable map zooming with mouse scroll when the user clicks the map
+    $('.embed-container').find('iframe').css("pointer-events", "none");
+    $('.embed-container').on('click', onMapClickHandler);
+
 });
